711b7f56806f006ff469da30db53703f4b7ee10e
jokerwhite2023 <sertq31@gmail.com>
2016-02-01 11:39:23 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {@jokerwhithe},--Sudo users
+    sudo_users = {120983806},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
